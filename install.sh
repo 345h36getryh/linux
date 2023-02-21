9ed70499f13fc8dccf0865ab1abf74c20ee6b2c4
345h36getryh <125980231+345h36getryh@users.noreply.github.com>
2023-02-21 14:16:58 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt full-upgrade
+reboot
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get full-upgrade
+reboot
+wget 'https://liquorix.net/install-liquorix.sh'
+sudo bash install-liquorix.sh
+reboot
+sudo apt install midnight git python3 pip fish htop build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev curl software-properties-common screen python3-pip -y
+reboot
